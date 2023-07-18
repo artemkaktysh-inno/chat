1732dfa43021cc5b721a74187bbf99a57ffba477
50ShadesOfCode <kaktymail@gmail.com>
2023-07-18 15:28:53 +0300

    Add routes.

--- navigation/lib/app_router/app_router.dart
+++ navigation/lib/app_router/app_router.dart
@@ -1,9 +1,17 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:home/home.dart';
 
 part 'app_router.gr.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Screen,Route',
-  routes: <AutoRoute>[],
+  routes: <AutoRoute>[
+    AutoRoute(
+      initial: true,
+      page: HomeScreen,
+      children: <AutoRoute>[],
+    ),
+  ],
 )
 class AppRouter extends _$AppRouter {}
--- navigation/lib/navigation.dart
+++ navigation/lib/navigation.dart
@@ -1,4 +1,6 @@
 library navigation;
 
+export 'package:auto_route/auto_route.dart';
+
 export 'app_router/app_router.dart';
 export 'di/navigation_di.dart';
